c416660cf0b9a0003db902762b982b4727d98887
André Abreu <22.01255-9@maua.br>
2025-07-26 15:05:59 -0300

    widget para cada entrada na linha do tempo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:andre_e_elisa/widgets/clock_widget.dart';
+import 'package:andre_e_elisa/widgets/timeline_entry_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,7 +13,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 222, 153, 220),
+      backgroundColor: Color.fromARGB(255, 218, 152, 206),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
@@ -23,11 +24,18 @@ class _HomePageState extends State<HomePage> {
                 style: TextStyle(color: Colors.white, fontSize: 24),
               ),
               ClockWidget(),
-              Image.asset(
-                'assets/images/parque-flamboyant.jpg',
-                width: 480,
-                height: 640,
+              SizedBox(height: 64),
+              TimelineEntryWidget(
+                date: '06/06/2025',
+                filename: 'pedido-de-namoro.jpg',
+                title: 'O dia do pedido',
               ),
+              TimelineEntryWidget(
+                date: '07/07/2025',
+                filename: 'parque-flamboyant.jpg',
+                title: 'Passeio no parque Flamboyant',
+              ),
+              SizedBox(height: 128),
             ],
           ),
         ),
--- lib/widgets/timeline_entry_widget.dart
+++ lib/widgets/timeline_entry_widget.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
 
 class TimelineEntryWidget extends StatelessWidget {
-  const TimelineEntryWidget({super.key});
+  final String date;
+  final String filename;
+  final String title;
+
+  const TimelineEntryWidget({
+    super.key,
+    required this.date,
+    required this.filename,
+    required this.title,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 800),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                date,
+                textAlign: TextAlign.right,
+                style: TextStyle(color: Colors.white, fontSize: 16),
+              ),
+            ],
+          ),
+          Container(height: 2, color: Colors.white),
+          SizedBox(height: 16),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Image.asset(
+                'assets/images/$filename',
+                width: 300,
+                height: 400,
+                fit: BoxFit.fitWidth,
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            title,
+                            style: TextStyle(color: Colors.white, fontSize: 24),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    const Text(
+                      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at eleifend velit. Ut cursus nibh quis ipsum laoreet elementum. Pellentesque at sem egestas, pellentesque ligula eget, tristique diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas vestibulum id nunc et iaculis. Sed elementum augue vitae nulla consequat, eget vehicula mi euismod. Integer nec velit in leo cursus lobortis quis non arcu. In hac habitasse platea dictumst. Duis venenatis rhoncus sapien sed ornare.',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
